f93c04133e148a2a65508eb2270eb3912625d1ee
Shasi Singh <shasi.singh@rabobank.nl>
2024-06-12 15:48:59 +0200

    util JwtGenerator to read signingKey key only 1 time

--- src/main/java/com/nightcrowler/spring/banking/security/JwtGenerator.java
+++ src/main/java/com/nightcrowler/spring/banking/security/JwtGenerator.java
@@ -3,6 +3,8 @@ package com.nightcrowler.spring.banking.security;
 import com.nightcrowler.spring.banking.controler.TokenGenerator;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
@@ -20,6 +22,16 @@ public class JwtGenerator {
     @Value("${security.secret:4f6db2d788a54a9cae790fabea3268f2}")
     private String secret;
 
+    private Key signingKey;
+
+    @PostConstruct
+    public void init() {
+        if (secret == null || secret.isEmpty()) {
+            throw new BeanInitializationException("Secret cannot be empty.");
+        }
+        signingKey = getSigningKey();
+    }
+
     public String generate(String userName, String role) {
         return doGenerateToken(userName, role);
     }
@@ -30,9 +42,9 @@ public class JwtGenerator {
                 .add("role", role)
                 .subject(username)
                 .issuedAt(Date.from(LocalDateTime.now().toInstant(ZoneOffset.UTC)))
-                .expiration(Date.from(LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC)))
+                .expiration(Date.from(LocalDateTime.now().plusMinutes(10).toInstant(ZoneOffset.UTC)))
                 .and()
-                .signWith(getSigningKey())
+                .signWith(signingKey)
                 .compact();
     }
 
