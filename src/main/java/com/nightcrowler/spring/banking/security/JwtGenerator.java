e0ec96ac2d6d3e083841e2e393c25f39373f21c4
Shasi Singh <shasi.singh@rabobank.nl>
2024-06-12 15:44:31 +0200

    @ConditionalOnBean(TokenGenerator.class)

--- src/main/java/com/nightcrowler/spring/banking/security/JwtGenerator.java
+++ src/main/java/com/nightcrowler/spring/banking/security/JwtGenerator.java
@@ -1,5 +1,6 @@
 package com.nightcrowler.spring.banking.security;
 
+import com.nightcrowler.spring.banking.controler.TokenGenerator;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,7 +14,7 @@ import java.time.ZoneOffset;
 import java.util.Date;
 
 @Component
-@ConditionalOnBean(JwtGenerator.class)
+@ConditionalOnBean(TokenGenerator.class)
 public class JwtGenerator {
 
     @Value("${security.secret:4f6db2d788a54a9cae790fabea3268f2}")
